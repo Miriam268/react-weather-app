64f44fd719a5c53167028a014d87b1a97747972f
Miriam268 <miriamkawondera@gmail.com>
2023-10-09 14:23:22 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./App.css" ;
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-    Weather App
+      <div className="container">
+   <h1>Weather App</h1>
     <footer>
       This project was coded by Miriam kawondera and is {" "}
       <a 
       href="https://github.com/Miriam268/weather-react-app" 
       target="_blank"
       >
-        open-sourced on github
+        open-sourced on GitHub
         </a>
         </footer>
+      </div>
       </div>
   );
 }
 
-export default App;
+
