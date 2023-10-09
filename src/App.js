4faee7cdca3fa6690afc203c8059ad9e1ade910c
Miriam268 <miriamkawondera@gmail.com>
2023-10-09 12:49:19 +0200

    Created Github repo Url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css" ;
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+    Weather App
+    <footer>
+      This project was coded by Miriam kawondera and is {" "}
+      <a 
+      href="https://github.com/Miriam268/weather-react-app" 
+      target="_blank"
+      >
+        open-sourced on github
+        </a>
+        </footer>
+      </div>
+  );
 }
 
 export default App;
